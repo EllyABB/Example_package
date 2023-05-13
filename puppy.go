77de1ca51e10a0709b304e50938607a6f0b4e252
EllyBB <ekbayonab@gmail.com>
2023-05-13 15:33:59 -0600

    Sub_pack

--- puppy.go
+++ puppy.go
@@ -1,9 +1,22 @@
 package puppy
 
+import(
+	"https://github.com/EllyABB/Example_sub_mod"
+)
+
 func Ladrar() string{
 	return "woff"
 }
 
 func Ladridos() string{
 	return "woff, woff, woff"
+}
+
+func La_que_llama_a_otra() string{
+	return sub_pack.Algo(Ladrar)
+}
+
+
+func La_que_llama_a_otra2() string{
+	return sub_pack.Algo(Ladridos)
 }
